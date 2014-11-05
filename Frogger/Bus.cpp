501dacb5c8399454d53f2bd83cf68315a7f14ee1
Xylios13 <gbarreto1992@gmail.com>
2014-11-05 16:27:19 +0000

    Removed Window from bus

--- Frogger/Bus.cpp
+++ Frogger/Bus.cpp
@@ -22,15 +22,16 @@ void Bus::create(VSMathLib* vsml, VSResSurfRevLib mySurfRev){
 	int texcount = 0;
 	
 	//mask
+	/*
 	mySurfRev.createCylinder(0.001f, 1.0f, 4);
 	mySurfRev.setColor(VSResourceLib::MaterialSemantics::AMBIENT, ambWindow);
 	mySurfRev.setColor(VSResourceLib::MaterialSemantics::DIFFUSE, diffWindow);
 	mySurfRev.setColor(VSResourceLib::MaterialSemantics::SPECULAR, specWindow);
 	mySurfRev.setColor(VSResourceLib::MaterialSemantics::EMISSIVE, emissive);
 	mySurfRev.setColor(VSResourceLib::MaterialSemantics::SHININESS, shininessWindow);
-
+	
 	objId++;
-
+	*/
 	mySurfRev.createCylinder(6.0f, 3.0f, 4);
 	mySurfRev.setColor(VSResourceLib::MaterialSemantics::AMBIENT, ambBody);
 	mySurfRev.setColor(VSResourceLib::MaterialSemantics::DIFFUSE, diffBody);
@@ -82,10 +83,11 @@ void Bus::draw(VSMathLib* vsml){
 
 	int currentObjId = Bus::busObjId;
 
-	glStencilFunc(GL_ALWAYS, 0x1, 0x1);
-	glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
+	//glStencilFunc(GL_ALWAYS, 0x1, 0x1);
+	//glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);
 
 	//mask
+	/*
 	vsml->pushMatrix(VSMathLib::MODEL);
 	vsml->translate(getX(), getY() - 2.0f, getZ() + 0.0f);
 	vsml->rotate(45.0f, 0, 1, 0);
@@ -97,82 +99,76 @@ void Bus::draw(VSMathLib* vsml){
 	currentObjId++;
 
 	vsml->popMatrix(VSMathLib::MODEL);
-
-	glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-	glStencilFunc(GL_NOTEQUAL, 0x1, 0x1);
+	*/
+	//glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+	//glStencilFunc(GL_NOTEQUAL, 0x1, 0x1);
 	vsml->pushMatrix(VSMathLib::MODEL);
-	if (isRevert()) {
-		vsml->scale(-1.0f, 1.0f, 1.0f);
-		isRevert(false);
-	}
-	vsml->translate(getX(), getY(), getZ() + 1.0f);
-	vsml->scale(1.0f, 1.0f, 0.7f);
-	vsml->rotate(90.0f, 0, 0, 1);
-	vsml->rotate(45.0f, 0, 1, 0);
+		if (isRevert()) {
+			vsml->scale(-1.0f, 1.0f, 1.0f);
+			isRevert(false);
+		}
+		vsml->translate(getX(), getY(), getZ() + 1.0f);
+		vsml->scale(1.0f, 1.0f, 0.7f);
+		vsml->rotate(90.0f, 0, 0, 1);
+		vsml->rotate(45.0f, 0, 1, 0);
 
-	initShadersVars(vsml, currentObjId);
-	glBindVertexArray(mesh[currentObjId].vao);
-	glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
+		initShadersVars(vsml, currentObjId);
+		glBindVertexArray(mesh[currentObjId].vao);
+		glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
 
-	glBindVertexArray(0);
-	currentObjId++;
+		glBindVertexArray(0);
+		currentObjId++;
 	vsml->popMatrix(VSMathLib::MODEL);
 	
-	glClearStencil(0x0);
-	glClear(GL_STENCIL_BUFFER_BIT);
-	//glStencilOp(GL_ALWAYS, 0x1, 0x1);
-	//glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+	//glClearStencil(0x0);
+	//glClear(GL_STENCIL_BUFFER_BIT);
 
 	vsml->pushMatrix(VSMathLib::MODEL);
-	vsml->translate(getX(), getY(), getZ() - 1.0f);
-	vsml->translate(1.7f, 2.0f, 0.2f);
+		vsml->translate(getX(), getY(), getZ() - 1.0f);
+		vsml->translate(1.7f, 2.0f, 0.2f);
 
-	initShadersVars(vsml, currentObjId);
-	glBindVertexArray(mesh[currentObjId].vao);
-	glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
+		initShadersVars(vsml, currentObjId);
+		glBindVertexArray(mesh[currentObjId].vao);
+		glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
 
-	glBindVertexArray(0);
-	currentObjId++;
-
+		glBindVertexArray(0);
+		currentObjId++;
 	vsml->popMatrix(VSMathLib::MODEL);
 
 	vsml->pushMatrix(VSMathLib::MODEL);
-	vsml->translate(getX(), getY(), getZ() - 1.0f);
-	vsml->translate(-1.7f, 2.0f, 0.2f);
+		vsml->translate(getX(), getY(), getZ() - 1.0f);
+		vsml->translate(-1.7f, 2.0f, 0.2f);
 
-	initShadersVars(vsml, currentObjId);
-	glBindVertexArray(mesh[currentObjId].vao);
-	glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
+		initShadersVars(vsml, currentObjId);
+		glBindVertexArray(mesh[currentObjId].vao);
+		glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
 
-	glBindVertexArray(0);
-	currentObjId++;
-
+		glBindVertexArray(0);
+		currentObjId++;
 	vsml->popMatrix(VSMathLib::MODEL);
 
 	vsml->pushMatrix(VSMathLib::MODEL);
-	vsml->translate(getX(), getY(), getZ() - 1.0f);
-	vsml->translate(1.7f, -2.0f, 0.2f);
+		vsml->translate(getX(), getY(), getZ() - 1.0f);
+		vsml->translate(1.7f, -2.0f, 0.2f);
 
-	initShadersVars(vsml, currentObjId);
-	glBindVertexArray(mesh[currentObjId].vao);
-	glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
+		initShadersVars(vsml, currentObjId);
+		glBindVertexArray(mesh[currentObjId].vao);
+		glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
 
-	glBindVertexArray(0);
-	currentObjId++;
-
+		glBindVertexArray(0);
+		currentObjId++;
 	vsml->popMatrix(VSMathLib::MODEL);
 
 	vsml->pushMatrix(VSMathLib::MODEL);
-	vsml->translate(getX(), getY(), getZ() - 1.0f);
-	vsml->translate(-1.7f, -2.0f, 0.2f);
+		vsml->translate(getX(), getY(), getZ() - 1.0f);
+		vsml->translate(-1.7f, -2.0f, 0.2f);
 
-	initShadersVars(vsml, currentObjId);
-	glBindVertexArray(mesh[currentObjId].vao);
-	glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
+		initShadersVars(vsml, currentObjId);
+		glBindVertexArray(mesh[currentObjId].vao);
+		glDrawElements(mesh[currentObjId].type, mesh[currentObjId].numIndexes, GL_UNSIGNED_INT, 0);
 
-	glBindVertexArray(0);
-	currentObjId++;
-
+		glBindVertexArray(0);
+		currentObjId++;
 	vsml->popMatrix(VSMathLib::MODEL);
 
 	
